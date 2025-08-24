604d854e479aabbcb9438a2eaaa252634705ccae
user <user@KEPLER-22B>
2025-08-24 22:13:21 +0800

    modified:   8w0.html 	modified:   simplex1.html 	modified:   slotmachine.html 	modified:   vis4d0.html 	new file:   vis4d1a.html 	modified:   ../src/MusicPlayer.jsx

--- src/MusicPlayer.jsx
+++ src/MusicPlayer.jsx
@@ -156,10 +156,9 @@ const MusicPlayer = () => {
         { text: "DS OR單形法", filename: "./simplex.html", isEnglish: false, isGame: true },
         { text: "Grok OR單形法", filename: "./simplex1.html", isEnglish: false, isGame: true },
 
-
-        { text: "", filename: "", isEnglish: false, isEmpty: true },
-        { text: "", filename: "", isEnglish: false, isEmpty: true },
-        { text: "", filename: "", isEnglish: false, isEmpty: true },
+        { text: "公車路線", filename: "./indexv12.html", isEnglish: false, isGame: true },
+        { text: "64卦狀態機", filename: "./yy.html", isEnglish: false, isGame: true },
+        { text: "八宮卦方立體", filename: "./zz.html", isEnglish: false, isGame: true },
         { text: "", filename: "", isEnglish: false, isEmpty: true },
 
         { text: "", filename: "", isEnglish: false, isEmpty: true },
@@ -185,7 +184,7 @@ const MusicPlayer = () => {
         { text: "🔺64D2", filename: "./vis_mobile.html", isEnglish: false, isGame: true },
         { text: "🎯3d", filename: "./vertices3d.html", isEnglish: false, isGame: true },
         { text: "👁vis4d", filename: "./vis4d.html", isEnglish: false, isGame: true },
-        { text: "👁vis4d0", filename: "./vis4d0.html", isEnglish: false, isGame: true },
+        { text: "👁vis4d1a", filename: "./vis4d1a.html", isEnglish: false, isGame: true },
         { text: "✨vis6e", filename: "vis6e.html", isEnglish: false, isGame: true },
         { text: "大運流年", filename: "8w2.html", isEnglish: false, isGame: true },
         { text: "拉霸機", filename: "slotmachine.html", isEnglish: false, isGame: true },
